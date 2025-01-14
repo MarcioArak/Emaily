239ce502afc45730ccb367113ffdfe3b1af5c267
Marcio Arakaki <marcio.arakaki@HPPFS.local>
2025-01-14 09:29:26 -0800

    Added billing and client side app

---
+++ client/src/components/App.js
@@ -0,0 +1,36 @@
+import "materialize-css/dist/css/materialize.min.css";
+import React, { Component } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import { connect } from "react-redux";
+import * as actions from "../actions";
+import Header from "./Header";
+import Landing from "./Landing";
+
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+
+class App extends Component {
+  componentDidMount() {
+    this.props.fetchUser();
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/surveys" component={Dashboard} />
+            <Route path="/surveys/new" component={SurveyNew} />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
+
+export default connect(null, actions)(App);
+
+// if a file is exporting a class or a react component label it with a capital letter
+// if the file returns just a function a just a series of functinos we will label it with a lower case letter
